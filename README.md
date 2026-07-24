b499276e98bc3c724f5984f547382858abd659b2
ShinySkink9185 <shinyskink9185@gmail.com>
2026-07-24 11:57:54 -0400

    Animation Work & README

---
+++ README.md
@@ -0,0 +1,140 @@
+PROJECT SHUFFLE - by ShinySkink9185
+
+You are free to use, modify, or suggest things; do whatever you want with this! Just make sure I (ShinySkink9185) am properly credited, as well as anyone who may have helped with code. (Though, I won't get angry if you don't; I'm not that type of guy!)
+
+PROJECT CREDITS:
+
+ShinySkink9185 (made the game)
+
+ASSISTANCE:
+kleonc
+
+ASSETS TAKEN FROM:
+Sonic Shuffle (Dreamcast)
+Ripped from The Spriters Resource
+Made by SEGA/Hudson Soft
+
+Sonic Advance 1-3 & Battle (GBA)
+Ripped from The Spriters Resource
+Made by SEGA/Sonic Team/Dimps
+
+SPECIAL THANKS (in alphabetical order):
+1337b2
+A'Dilla
+AaRon The Human
+Adude
+Aimeers
+Annikku
+AStrangerInTheObjectCommunity
+Bandai Namco
+Becky
+bitten2up
+Blazy Monodia
+Bluelight
+BluJayCherry
+Волфганг
+Boomer
+Bugs
+Cable
+CallieMacN
+Cassedila
+Chuckles_troll
+Chungus Bungus
+Cobaltn't
+CosmicBrie
+Cosmid
+Curedleaves
+Dakkī the RoBo
+Darko/Mlgjohn
+Dark the Hedgehog
+DaStupidSonic
+Diligentchi
+Evil Dark the Hedgehog
+Flame the Synth-Landit
+FleetLovesPizza
+FlitVerbal
+FlowerGirl
+Future Smiles
+Gabri.lua
+GalaxyTrail
+Giberish4
+Goro
+Greennick78sh
+Gumpy63
+HCBR
+Hezi
+HyperPotato
+Hydro
+Isabelle
+Limo
+Jaden
+JackieJelly
+Jisk
+Joten/Idot
+Justkenji1
+KaiyoLazr
+Kart Krew Dev
+Katzy.mdg
+Kori
+Kilo
+KixxReborn
+KiyanTheBluePenguin
+MarieLilypad !!
+Marilyn
+MarleDrawsStuff
+MasochMoon
+Milk
+MisterDJ
+MrPastin
+Mr. Space
+M.T.O.V
+Mute
+Nessie
+OGAmyDroid
+OhThatGuy64
+OVA Pico
+OVATails
+Past Warrior
+Peppermint
+Pikzelow
+Potts
+Princess-Yahaira
+ProjectSNT
+Rakcid
+Rascally Rosy
+RedHotSonic
+Roc
+Rosie Kitsune
+RubyTheMii
+Ryuuji Kohsinski Shinigami
+SandwichCinema
+SaturnDreemurr32X
+SEGA
+SethTheEchidna
+SeventhSentinel
+Skoopy
+Sling1991
+Slunky
+Snoccy
+Sonic Team
+Sonic Team Jr.
+Sonicspeed148
+Spectra
+Spinsei
+SplitsTheOne
+Spoonise
+Starlyn
+StarManiaKG
+SteveMonke
+Steven
+SuperKeanu
+Survival46
+SweetCoffy
+Tech
+The SRB2 Community
+Virmonth
+Weegs
+TheWhatParting
+wye
+Yume
+And plenty more...
--- Scenes/Objects/Player/player.gd
+++ Scenes/Objects/Player/player.gd
@@ -7,10 +7,17 @@ var playerID: int = 0 ## What player is attached to this guy?
 var state: String ## What state are we in?
 
 @onready var sprite = $Sprite3D
-@onready var animation = $Sprite3D/AnimationPlayer
+@onready var animationPlayer = $Sprite3D/AnimationPlayer
 
 func _ready():
 	refresh_animations()
+	animationPlayer.play("stand")
+	# TODO: fix Sonic's stand animation (his first is the same as one of his others, messing up timing)
+	# TODO: make animations loop once finished!
+
+func play_animation():
+	# TODO: play the animation if it exists; if it doesn't, fall back to the next on the list
+	pass
 
 func refresh_animations(): ## Refreshes all animations when changing characters.
 	# First, we need to check if our character is valid.
@@ -33,7 +40,59 @@ func refresh_animations(): ## Refreshes all animations when changing characters.
 	var characterImage = load(usedCharacter.image)
 	sprite.texture = characterImage
 	sprite.hframes = characterImage.get_width() / usedCharacter.boxWidth
-	sprite.hframes = characterImage.get_height() / usedCharacter.boxHeight
+	sprite.vframes = characterImage.get_height() / usedCharacter.boxHeight
 	
 	# TODO: add animations
+	# This is the tricky part; actually adding our animations!
 	
+	# First things first, we've gotta reset the AnimationPlayer's current animations.
+	var globalLibrary = animationPlayer.get_animation_library("")
+	
+	while animationPlayer.get_animation_list():
+		globalLibrary.remove_animation(animationPlayer.get_animation_list()[0])
+	
+	var animationIndex = 0
+	
+	animationPlayer.set_root_node(get_path())
+	
+	while animationIndex <= usedCharacter.animations.size() - 1:
+		# Define our data for ease of use
+		var id = usedCharacter.animations[animationIndex].id
+		var frames = usedCharacter.animations[animationIndex].frames 
+		var frameTimes = usedCharacter.animations[animationIndex].frameTimes
+		
+		# Add our animation, then set its initial properties.
+		var animation = Animation.new()
+		
+		var length = 0
+		for frameTime in frameTimes:
+			length += frameTime
+		animation.length = length
+		
+		# Get our tracks
+		var xFrameCoord = animation.add_track(Animation.TYPE_VALUE)
+		animation.track_set_path(xFrameCoord, "Sprite3D:frame_coords:x")
+		animation.track_set_interpolation_type(xFrameCoord, Animation.INTERPOLATION_NEAREST)
+		
+		var yFrameCoord = animation.add_track(Animation.TYPE_VALUE)
+		animation.track_set_path(yFrameCoord, "Sprite3D:frame_coords:y")
+		animation.track_set_interpolation_type(yFrameCoord, Animation.INTERPOLATION_NEAREST)
+		
+		# We just need to do the Y frame coord once...
+		animation.track_insert_key(yFrameCoord, 0.0, animationIndex)
+		
+		# For the X, however, we've gotta do that for all of our frames!
+		var frameIndex = 0
+		var currentFrameTime = 0
+		while frameIndex <= frames.size() - 1:
+			# TODO: fix the timing of this thing!
+			animation.track_insert_key(xFrameCoord, currentFrameTime, frames[frameIndex])
+			print(currentFrameTime * 60)
+			currentFrameTime += frameTimes[frameIndex]
+			frameIndex += 1
+		
+		# And finally, we add our animation!!
+		globalLibrary.add_animation(id, animation)
+		
+		animationIndex += 1
+	print(animationPlayer.get_animation_list())
