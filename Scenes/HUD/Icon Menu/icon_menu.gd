eb7f2fcaaa126f29d921cc8c5ca33cc95f7fad69
ShinySkink9185 <112836369+ShinySkink9185@users.noreply.github.com>
2026-08-25 22:16:42 -0400

    Icon Menu Desktop Transfer

--- Scenes/HUD/Icon Menu/icon_menu.gd
+++ Scenes/HUD/Icon Menu/icon_menu.gd
@@ -1,6 +1,16 @@
 extends Control
 
-enum IconMenuTypes {TYPE_BOARD, TYPE_SETTINGS, TYPE_BATTLE}
+enum IconMenuTypes {BOARD, SETTINGS, BATTLE}
 
 # TODO: Have the different types of menus show up in-game.
 # TODO: Have players be able to control it!
+
+var menu_ready = false # Is the menu ready to display options and handle input?
+var option_selected = 0 # What option have we selected in this menu?
+
+@export var type: IconMenuTypes
+
+func _process(delta: float) -> void:
+	if type == IconMenuTypes.BOARD:
+		# TODO: do stuff
+		pass
