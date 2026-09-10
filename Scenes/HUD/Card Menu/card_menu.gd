07eb1f7fd3f5bf9529cd10392502345ce62cbf56
ShinySkink9185 <112836369+ShinySkink9185@users.noreply.github.com>
2026-09-09 22:20:30 -0400

    Animations for Card Menu

--- Scenes/HUD/Card Menu/card_menu.gd
+++ Scenes/HUD/Card Menu/card_menu.gd
@@ -1,22 +1,25 @@
 extends CanvasLayer
 
-@onready var player_1_icon = $Player1/Icon
+# We put the icons inside the border because animations weren't cooperating with
+# changing the position of the stored "Player" nodes, but did cooperate with the
+# border for some reason
+@onready var player_1_icon = $Player1/Border/Icon
 @onready var player_1_border = $Player1/Border
 
-@onready var player_2_icon = $Player2/Icon
+@onready var player_2_icon = $Player2/Border/Icon
 @onready var player_2_border = $Player2/Border
 
-@onready var player_3_icon = $Player3/Icon
+@onready var player_3_icon = $Player3/Border/Icon
 @onready var player_3_border = $Player3/Border
 
-@onready var player_4_icon = $Player4/Icon
+@onready var player_4_icon = $Player4/Border/Icon
 @onready var player_4_border = $Player4/Border
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Replace our icon.
 	# TODO: do that
-	
+	pass
 	
 	# Change the color of our border.
 	# TODO: do that
